a0f5b8ae75da66016fbec45ba073426051e7ae04
SantiagoEche0799 <santiago.eche.duque.99910@gmail.com>
2021-06-01 16:25:41 -0500

    project init

---
+++ src/main/java/co/edu/usbbog/bdd/ApiBancoDatacenterApplication.java
@@ -0,0 +1,13 @@
+package co.edu.usbbog.bdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBancoDatacenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBancoDatacenterApplication.class, args);
+	}
+
+}
